0cd8298b788bd04fa3b1add7d7f06c681fceb9fb
jvmqueue <wheels0090@yahoo.com>
2015-02-01 09:59:15 -0500

    cleaner method using Xml object, along with triggerHandler

--- scripts/import.js
+++ scripts/import.js
@@ -1,84 +1,51 @@
-/*		<div class="ontainerMiddleCenter center">
-			<h1 class="middleCenter white">BUISINESS <span class="gray">TIME</span></h1>
-			<h2 class="middleCenter gray">FRESH IDEAS FOR YOUR BUSINESS</h2>
-		</div>*/
-
 var jvm = jvm || {};
-jvm.importData = (function(w, d, $){
+jvm.import = (function(w, d, $){
 
-
-	var Data = function(options){
-		/*this.url = options.url;*/
-		
-		this.url = options.url;
-		this.xmlFrag = 'Hello World';
+	var Page = function(paramXmlObject){
 
 	};
-	Data.prototype = {
-		that:this,
-		connect:function(){
-			
-			var _frag = 'Hello World';
-			var xmlRequest = $.ajax({
-				url:this.url,
-				context:d.body
-			});
-			xmlRequest.done(
-				function(data){				
-				_frag = data.getElementsByTagName('fragment')[0];
-				$('#container').triggerHandler('data:retrieved', [_frag]);
-			});	
-			xmlRequest.always(function(error){
-				console.log('Error:\t', error);
-			});
+
+	var Xml = function(){
+
+	};
+
+	Xml.prototype = {
+		getNode:function(paramNodeName){
+			var node = null;
+			return node;
 		},
-		get:function(){},
-		append:function(options, paramXmlData){			
-
-
-
-
+		getResponse:function(paramUrl){
+			var req = $.ajax({
+				url:paramUrl,
+				context:d.body,
+			});
+			req.done(
+				function(data){
+					$('#container').triggerHandler('data:retrieved', [data]);
+				}
+			);
+		},
+		saveResponse:function(){
 			
-			var frag = d.createDocumentFragment();
-			var firstChild = paramXmlData.getElementsByTagName('div')[0];
-			var nodeHtml = d.createElement( firstChild.nodeName );
-			nodeHtml.setAttribute( 'class', firstChild.getAttribute('class') );
-			var nodeHeading0 = d.createElement(firstChild.getElementsByTagName('h1')[0].nodeName);
-			var nodeHeading0Attribute = (firstChild.getElementsByTagName('h1')[0].getAttribute('class'));
-			
-			var nodeHeading0Text = d.createTextNode(firstChild.getElementsByTagName('h1')[0].firstChild.nodeValue);
-			
-			nodeHeading0.setAttribute('class', nodeHeading0Attribute);
-			nodeHeading0.appendChild(nodeHeading0Text);
-			frag.appendChild(nodeHeading0);
-			
-
-			var nodeExist = d.getElementById('container');
-			nodeExist.appendChild(frag);
-
-
-
-			
-		}
+		},
+		listener:function(e, paramData){
+			console.group();
+				console.log('Reached Event listener paramData:\t', paramData);
+			console.groupEnd();	
+		}		
 	};
 
-	var doData = function(object){
-		jvm.Interface.ensureImplements(object, InterfaceData);
-		var lclXmlData = object.connect();
-	};
-	var InterfaceData = new jvm.Interface('data', ['get', 'append', 'connect']);
-	// define main() for init controller
-	// main() involks doData(pass in data JS object) 
-	// - - > test data JS object against InterfaceData
 
+
+	// main page init entry point
 	var main = (function(){
-		var objectData = new Data({url:'../pageMiddleRow0/index.xml'});
-
-		$(function(){ // wait for DOM
-			$('#container').on('data:retrieved', objectData.append); // define listener. We trigger it once AJAX response received			
-			doData(objectData);			
-		});
+		var objXml = new Xml();
 		
+		$(function(){ // wait for DOM
+			$('#container').on('data:retrieved', objXml.listener);
+			objXml.getResponse('../pageMiddleRow0/index.xml');
+		});
+
 	})();
 
 })(window, document, jQuery);
